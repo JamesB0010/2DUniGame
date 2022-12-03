1d027d7f998484769c55750efbeda487bbf567dd
James Bland <jammydodger8888@gmail.com>
2022-12-03 14:49:42 +0000

    Progress on upgrade system

--- 2D game/Assets/Scripts/UpgradeSlot.cs
+++ 2D game/Assets/Scripts/UpgradeSlot.cs
@@ -13,25 +13,25 @@ public class UpgradeSlot : MonoBehaviour, IPointerClickHandler
     {
         Debug.Log("Ive been clicked");
         popUpWindow.GetComponent<Image>().enabled = true;
-        floodHide(popUpWindow);
+        floodHideShow(popUpWindow, true);
     }
 
-    private void floodHide(GameObject root)
+    public void floodHideShow(GameObject root, bool show)
     {
         for (int i = 0; i < root.transform.childCount; i++)
         {
             if (root.transform.GetChild(i).GetComponent<Image>() != null)
             {
-                root.transform.GetChild(i).GetComponent<Image>().enabled = true;
+                root.transform.GetChild(i).GetComponent<Image>().enabled = show;
 
             }
             if (root.transform.GetChild(i).GetComponent<TextMeshProUGUI>() != null)
             {
-                root.transform.GetChild(i).GetComponent<TextMeshProUGUI>().enabled = true;
+                root.transform.GetChild(i).GetComponent<TextMeshProUGUI>().enabled = show;
             }
             if (root.transform.GetChild(i).transform.childCount > 0)
             {
-                floodHide(root.transform.GetChild(i).gameObject);
+                floodHideShow(root.transform.GetChild(i).gameObject, show);
             }
         }
     }
